674e121e94839642b09a2ac300cc71492f0660db
luuufan <angelia.fan@gmail.com>
2018-05-29 14:40:31 -0700

    adding input into main.html, trying to check link availability

--- linkify_content.js
+++ linkify_content.js
@@ -1,7 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const mainDiv = document.querySelector('.main div');
-  convertLink(mainDiv);
+  const body = document.querySelector('body');
+  convertLink(body);
+  
+  const form = document.querySelector("#blog-input");
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    submitForm();
+  });
+
+  form.addEventListener('keypress', function(){
+    const hint = document.querySelector('.hint');
+    if (hint.classList.length < 2) {
+      hint.classList.add('hide');
+    }
+  });
+  
+  // document.addEventListener('DOMNodeInserted', function(){
+  //   const wholeContent = document.querySelector('body');
+  //   convertLink(wholeContent);
+  // });
 });
+
 
 const convertLink = (el) => {
   if (el.childNodes.length === 1 && el.childNodes[0].nodeType === 3) {
@@ -21,5 +40,45 @@ const convertLink = (el) => {
 const replaceLink = (text) => {
   const regex = /(^|[^@\w])@(\w{1,15})\b/g;
   const replace = '$1<a href="https://twitter.com/$2" target="_blank">@$2</a>';
-  return text.replace(regex, replace);
+  return text.replace(regex, replace);  
+  // const handlerList = text.match(regex);
+  // if (handlerList && handlerList.length){
+  //   handlerList.forEach(handler => {
+          // const link = 'https://twitter.com/users/username_available?context=signup&custom=true&email=&full_name=&suggest=1&suggest_on_username=true&username=';
+  //     if (checkLink(`${link}${handler.trim().slice(1)}`)) {
+  //       const replace = '$1<a href="https://twitter.com/$2" target="_blank">@$2</a>';
+  //       text.replace(handler, replace);
+  //     }
+  //   });
+  // } 
+  // return text;
+};
+
+const checkLink = (url) => {
+  const request = new Request(url);
+  fetch(request)
+    .then((res)=>{
+      debugger
+    })
+    .catch((err)=>{
+      debugger
+    });  
+};
+
+const submitForm = () => {
+  const text = document.querySelector("textarea");
+  if (text.value) {
+    appendBlog(text.value);
+    text.value = "";
+  } else {
+    const hint = document.querySelector('.hint');
+    hint.classList.remove('hide');
+  }
+}; 
+
+const appendBlog = (text) => {
+  const blog = document.querySelector(".blog-container");
+  const newBlog = document.createElement('div');
+  newBlog.innerHTML = text;
+  blog.prepend(newBlog);
 };
